1db0a323bd8549e4a5e6cb2eca7f5ae5692d5e4b
Daniela S. Gutiérrez Cortez <stephanny040807@gmail.com>
2026-03-09 17:42:06 -0600

    Add Proveedor class with attributes and methods

--- src/Problema/Proveedor.java
+++ src/Problema/Proveedor.java
@@ -1,4 +1,32 @@
 package Problema;
 
 public class Proveedor {
+    private String nombre;
+    private String telefono;
+    private String direccion;
+
+    public Proveedor(String nombre, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+    @Override
+    public String toString() {
+        return "Proveedor:" + "\n" +
+                "Nombre:" + nombre + "\n" +
+                "Telefono: " + telefono + "\n" +
+                "Direccion:'" + direccion + "\n";
+    }
 }
